78e2d18b0a93a80320f1fbef38457f46ff7ab944
nathan <nathanzam@gmail.com>
2022-10-25 18:59:35 -0400

    more carrie

--- src/app/components/carrie/carrie.component.ts
+++ src/app/components/carrie/carrie.component.ts
@@ -17,7 +17,9 @@ export class CarrieComponent implements OnInit {
     'https://media4.giphy.com/media/l0MYuogw2hfCk9UI0/giphy.gif?cid=ecf05e477tv29b9q886cekk2506see0mwa0euqj9orlou8ht&rid=giphy.gif&ct=g',
     'https://media4.giphy.com/media/qKDFrv7zdIUAo/giphy.gif?cid=ecf05e4758y4r2d6dod3qefyayh97vqtc1d5gc3mfelvvpfh&rid=giphy.gif&ct=g',
     'https://media3.giphy.com/media/UGWvlN1BfwzPa/giphy.gif?cid=790b7611a570b871d5e5576b2652d823d02d4a750e0069a5&rid=giphy.gif&ct=g',
-    'https://media1.giphy.com/media/l0MYMMW0uhzTsuZVe/giphy.gif?cid=790b7611e0eabf5a9f7c2f027e67442360967923ffafe2c8&rid=giphy.gif&ct=g'
+    'https://media1.giphy.com/media/l0MYMMW0uhzTsuZVe/giphy.gif?cid=790b7611e0eabf5a9f7c2f027e67442360967923ffafe2c8&rid=giphy.gif&ct=g',
+    'https://thumbs.gfycat.com/DisguisedDisfiguredAsianpiedstarling.webp',
+    'https://media.tenor.com/cLRMzr3ebPkAAAAM/4th-carrie-underwood.gif'
   ]
 
   constructor() { }
